c15015b0c847f74f3804914756b35fa2dc8c1911
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2024-04-16 23:16:03 +0100

    Finalização da estilização para mobile

--- src/Routes/Header/Header.tsx
+++ src/Routes/Header/Header.tsx
@@ -1,13 +1,13 @@
-import "./Header.module.scss";
+import styles from "./Header.module.scss";
 
 import icon_search from "/src/assets/icon_search.png";
 
 const Header = () => {
   return (
-    <header>
-        <h1>Code<span>Blog</span></h1>
-        <div>
-            <img src={icon_search}/>
+    <header className={styles.header}>
+        <h1 className={styles.title}><span className={styles.code}>Code</span>lândia</h1>
+        <div className={styles.box_search}>
+            <img src={icon_search} alt="Search"/>
             <input type="text" placeholder="Pesquisar no blog"/>
         </div>
     </header>
