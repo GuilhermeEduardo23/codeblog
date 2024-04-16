9e5b9944f8b0daed16353db112e8857bcf60a25a
Guilherme Eduardo <88467676+GuilhermeEduardo23@users.noreply.github.com>
2024-04-16 15:39:59 +0000

    Continuação da estilização dos componentes

--- src/Routes/Header/Header.tsx
+++ src/Routes/Header/Header.tsx
@@ -1,9 +1,11 @@
+import "./Header.module.scss";
+
 import icon_search from "/src/assets/icon_search.png";
 
 const Header = () => {
   return (
     <header>
-        <h1>CodeBlog</h1>
+        <h1>Code<span>Blog</span></h1>
         <div>
             <img src={icon_search}/>
             <input type="text" placeholder="Pesquisar no blog"/>
